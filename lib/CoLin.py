50c1b00f906d02bc40b1518f44de5ffec11a5c13
Qingyun Wu <qw2ky@virginia.edu>
2017-04-18 00:43:41 -0400

    add alpha_t

--- lib/CoLin.py
+++ lib/CoLin.py
@@ -8,6 +8,8 @@ from util_functions import vectorize, matrixize
 
 class CoLinUCBUserSharedStruct(object):
 	def __init__(self, featureDimension, lambda_, userNum, W):
+		self.currentW = np.identity(n = userNum)
+
 		self.W = W
 		self.userNum = userNum
 		self.A = lambda_*np.identity(n = featureDimension*userNum)
@@ -20,6 +22,10 @@ class CoLinUCBUserSharedStruct(object):
 		self.BigW = np.kron(np.transpose(W), np.identity(n=featureDimension))
 
 		self.CCA = np.dot(np.dot(self.BigW , self.AInv), np.transpose(self.BigW))
+
+		self.alpha_t = 0.0
+		self.sigma = 1.e-200   #Used in the high probability bound, i.e, with probability at least (1 - sigma) the confidence bound. So sigma should be very small
+		self.lambda_ = lambda_
 	def updateParameters(self, articlePicked, click,  userID, update):
 		pass
 	
@@ -31,13 +37,20 @@ class CoLinUCBUserSharedStruct(object):
 		
 		mean = np.dot(self.CoTheta.T[userID], article.contextFeatureVector)	
 		var = np.sqrt(np.dot(np.dot(TempFeatureV, self.CCA), TempFeatureV))
+
+
+		self.alpha_t = 0.01*np.sqrt(np.log(np.linalg.det(self.A)/float(self.sigma * self.lambda_) )) + np.sqrt(self.lambda_)
 		
-		pta = mean + alpha * var
+		#pta = mean + alpha * var    # use emprically tuned alpha
+		pta = mean + self.alpha_t *var   # use the theoretically computed alpha_t
 		
 		return pta
 
 class AsyCoLinUCBUserSharedStruct(CoLinUCBUserSharedStruct):	
-	def updateParameters(self, articlePicked, click,  userID, update='Inv'):	
+	def updateParameters(self, articlePicked, click,  userID, update='Inv'):
+
+		#self.currentW = np.dot(self.currentW, self.W)
+		#X = vectorize(np.outer(articlePicked.contextFeatureVector, self.currentW.T[userID])) 	
 		
 		X = vectorize(np.outer(articlePicked.contextFeatureVector, self.W.T[userID])) 
 		self.A += np.outer(X, X)	
@@ -140,6 +153,7 @@ class CoLinUCB_SelectUserAlgorithm:
 		self.dimension = dimension
 		self.alpha = alpha
 		self.W = W
+
 
 	def decide(self, pool_articles, AllUsers):
 		maxPTA = float('-inf')
