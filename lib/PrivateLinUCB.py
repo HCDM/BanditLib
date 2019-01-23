c07b11385be70dc65e9d9001010d855607f1aaea
davidbzhao <dz6hu@virginia.edu>
2019-01-22 23:22:13 -0500

    Calculate gaussian variance from epsilon and delta

--- lib/PrivateLinUCB.py
+++ lib/PrivateLinUCB.py
@@ -2,12 +2,13 @@ import numpy as np
 from util_functions import vectorize
 from Recommendation import Recommendation
 from BaseAlg import BaseAlg
+from scipy.stats import wishart
 """Paper: Differentially Private Contextual Bandits
 """
 
 
 class PrivateLinUCBUserStruct:
-    def __init__(self, featureDimension, lambda_, eps, testing_iterations, variance, protect_context, noise_type, init="zero"):
+    def __init__(self, featureDimension, lambda_, hyperparameters, protect_context, noise_type, init="zero"):
         """In the paper, V = A, u = b
         """
         self.d = featureDimension
@@ -15,14 +16,18 @@ class PrivateLinUCBUserStruct:
         self.V = self.M[:self.d, :self.d]
         self.u = self.M[:self.d, -1]
         self.Vinv = np.linalg.inv(self.V)
-        self.Zs = []
-        self.T = testing_iterations
-        self.eps = eps
-        self.variance = variance  # TODO: calculate from epsilon
+        self.T = hyperparameters['T']
+        self.eps = hyperparameters['eps']
+        self.delta = hyperparameters['delta']
+
+        # TODO: calculate from epsilon
+        self.df = hyperparameters['df']
+        self.scale = hyperparameters['scale']
+
         self.protect_context = protect_context
         self.noise_type = noise_type.lower()
 
-        if (init == "random"):
+        if init == "random":
             self.UserTheta = np.random.rand(self.d)
         else:
             self.UserTheta = np.zeros(self.d)
@@ -31,21 +36,33 @@ class PrivateLinUCBUserStruct:
     def updateParameters(self, articlePicked_FeatureVector, click):
         change = np.outer(articlePicked_FeatureVector,
                           articlePicked_FeatureVector)
-
+        # Calculate noise
         num_partial_sums = bin(self.time).count('1')
         N = np.zeros(shape=(self.d + 1, self.d + 1))
-        # Calculate noise
         if self.noise_type == 'gaussian':
+            # For testing, try T = 200, delta = 5.6, eps = 75
+            m = int(np.ceil(np.log2(self.T))) + 1  # max_number_of_p_sums
+            max_feature_vector_l2_norm = 1  # assumed, but noise added to reward might be problematic
+            max_reward_l1_norm = 1  # assumed
+            L_tilde = np.sqrt(max_feature_vector_l2_norm**2 + max_reward_l1_norm**2)
+            variance = 16 * m * L_tilde**4 * np.log(4 / self.delta)**2 / self.eps**2
             for _ in range(num_partial_sums):
-                Z = np.random.normal(scale=np.sqrt(self.variance),
+                Z = np.random.normal(scale=np.sqrt(variance),
                                     size=(self.d + 1, self.d + 1))
                 N += (Z + Z.T) / np.sqrt(2)
         elif self.noise_type == 'laplacian':
             for _ in range(num_partial_sums):
+                #  Paper: Differential privacy under continual observation says add Lap(logT/e)
+                #       Lap(scale=x) + Lap(scale=x) has a lower variance than Lap(scale=2x)
+                #           So why not just add Lap(scale=1/eps) to each reward and then sum them up?
+                #           Wouldn't that be more effective than adding one big lump sum of Lap(scale=T/eps)?
+                #       Is noise resampled every time for each node or is it sampled once and stored?
+                #           Does it make a difference?
                 N += np.random.laplace(scale=np.log(self.T) / self.eps,
                                     size=(self.d + 1, self.d + 1))
         elif self.noise_type == 'wishart':
-            raise NotImplementedError()
+            for _ in range(num_partial_sums):
+                N += wishart.rvs(self.df, self.scale, size=(self.d + 1, self.d + 1))
         else:
             raise NotImplementedError()
 
@@ -55,11 +72,10 @@ class PrivateLinUCBUserStruct:
         self.M += np.outer(action_and_reward_vector, action_and_reward_vector)
 
         # Calculate user theta
-        if self.protect_context:
-            self.V = (self.M + N)[:self.d, :self.d]
+        self.V = (self.M + N)[:self.d, :self.d]
+        if self.protect_context:  # NIPS
             self.u = (self.M + N)[:self.d, -1]
         else:  # ICML
-            self.V = (self.M + N)[:self.d, :self.d]
             self.u = self.M[:self.d, -1]
         self.Vinv = np.linalg.inv(self.V)
         self.UserTheta = np.dot(self.Vinv, self.u)
@@ -92,14 +108,19 @@ class PrivateLinUCBAlgorithm(BaseAlg):
     def __init__(self, arg_dict, init="zero"):  # n is number of users
         BaseAlg.__init__(self, arg_dict)
         self.users = []
+        hyperparameters = {
+            'eps': arg_dict['eps'],
+            'delta': arg_dict['delta'],
+            'T': arg_dict['T'],
+            'df': arg_dict['df'],
+            'scale': arg_dict['scale']
+        }
         # algorithm have n users, each user has a user structure
         for i in range(arg_dict['n_users']):
             self.users.append(PrivateLinUCBUserStruct(
                 arg_dict['dimension'],
                 arg_dict['lambda_'],
-                arg_dict['eps'],
-                arg_dict['T'],
-                arg_dict['variance'],
+                hyperparameters,
                 arg_dict['protect_context'],
                 arg_dict['noise_type'],
                 init))
