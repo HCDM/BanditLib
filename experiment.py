da036b7a19a7e62f170f1a47eeb16370d4824a64
davidbzhao <dz6hu@virginia.edu>
2019-05-16 17:21:51 -0400

    Save number of arm changes

--- experiment.py
+++ experiment.py
@@ -73,6 +73,24 @@ def print_summary():
     print('Summary')
 
 
+def compute_arm_diffs(orig_arm_file, new_arm_file):
+    with open(orig_arm_file, 'r') as infile:
+        orig_aids = [line.split(',') for line in infile.readlines()]
+    
+    with open(new_arm_file, 'r') as infile:
+        new_aids = [line.split(',') for line in infile.readlines()]
+    
+    num_diffs = 0
+    for i in range(len(orig_aids)):
+        for j in range(len(orig_aids[0])):
+            if orig_aids[i][j] != new_aids[i][j]:
+                num_diffs += 1
+    return num_diffs
+
+def save_changes(arm_changes_filepath, resample_round, resample_change, num_arm_changes):
+    with open(arm_changes_filepath, 'a+') as outfile:
+        outfile.write('{},{},{}\n'.format(resample_round, resample_change, num_arm_changes))
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description = '')
     parser.add_argument('--config', dest='config', help='yaml config file')
@@ -81,14 +99,16 @@ if __name__ == '__main__':
 
     config_filepath = args.config
     config = read_config(config_filepath)
+    experiment = config['experiment']
+    tmp_dir = experiment['tmp_directory'] if experiment.has_key('tmp_directory') else 'tmp'
 
     # Delete tmp
     if args.clean:
-        delete_tmp_directory('tmp')
+        delete_tmp_directory(tmp_dir)
 
     # Create tmp
-    create_tmp_directory('tmp')
-    tmp_exp_config_path = os.path.join('tmp', 'tmp.exp.yaml')
+    create_tmp_directory(tmp_dir)
+    tmp_exp_config_path = os.path.join(tmp_dir, 'tmp.exp.yaml')
 
     # Run once to generate arms, pools, etc. as specified
     config = prepare_config(config)
@@ -98,13 +118,24 @@ if __name__ == '__main__':
         run_simulation(tmp_exp_config_path)
 
     config = set_config_only_loads(config)
-    if not os.path.isfile('tmp/article_selection_history_original.csv'):
+    write_config(tmp_exp_config_path, config)
+    art_sel_hist_file = experiment['article_selection_file']
+    original_art_sel_hist_file = os.path.join(tmp_dir, 'article_selection_history_original.csv')
+    if not os.path.isfile(original_art_sel_hist_file):
         run_simulation(tmp_exp_config_path)
-        rename_article_history_csv('tmp/article_selection_history.csv', 'tmp/article_selection_history_original.csv')
+        rename_article_history_csv(art_sel_hist_file, original_art_sel_hist_file)
 
-    for n in range(0, 100, 100):
-        config = set_resample(config, n, 5)
+    resample_round_interval = experiment['resample']['round_interval']
+    resample_change = experiment['resample']['change']
+    arm_changes_filepath = experiment['arm_changes_file']
+    save_changes(arm_changes_filepath, 'Round', 'Change', 'DiffArms')
+
+    for n in range(0, config['general']['testing_iterations'] + 1, resample_round_interval):
+        config = set_resample(config, n, resample_change)
         write_config(tmp_exp_config_path, config)
         run_simulation(tmp_exp_config_path)
+
+        arm_changes = compute_arm_diffs(original_art_sel_hist_file, art_sel_hist_file)
+        save_changes(arm_changes_filepath, n, resample_change, arm_changes)
 
     print_summary()
