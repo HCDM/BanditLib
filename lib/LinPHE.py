eb4c091dc4889f1960042f004b588a27287279cd
OwenHG <ohg4dm@virginia.edu>
2020-02-10 13:54:44 -0500

    Update LinPHE to be able to choose k top articles

--- lib/LinPHE.py
+++ lib/LinPHE.py
@@ -10,7 +10,7 @@ class LinPHEUserStruct:
                 self.a = a
                 self.f = np.zeros(self.d)
                 self.B = np.zeros((self.d, self.d))
-                self.theta = np.zeros(self.d)
+                self.UserTheta = np.zeros(self.d)
                 self.G_additive = lambda_*(a+1)*np.identity(self.d)
 
         def updateParameters(self, articlePicked_featureVector, click):
@@ -19,10 +19,10 @@ class LinPHEUserStruct:
 
                 perturbed_reward = .1*np.random.binomial(self.a, .5)
                 self.f += articlePicked_featureVector * (click+ perturbed_reward)
-                self.theta = np.dot(np.linalg.inv(G), self.f)
+                self.UserTheta = np.dot(np.linalg.inv(G), self.f)
 
         def getProb(self, article_featureVector):
-                return np.dot(self.theta, article_featureVector)
+                return np.dot(self.UserTheta, article_featureVector)
 
 
 class LinPHEAlgorithm(BaseAlg):
@@ -33,7 +33,7 @@ class LinPHEAlgorithm(BaseAlg):
                 for i in range(arg_dict['n_users']):
                         self.users.append(LinPHEUserStruct(arg_dict['dimension'], arg_dict['lambda_'], arg_dict['a']))                
 
-        def decide(self, pool_articles, userID, k=1):
+        def decide_old(self, pool_articles, userID, k=1):
                 maxPTA = float('-inf')
                 articlePicked = None
     
@@ -44,8 +44,25 @@ class LinPHEAlgorithm(BaseAlg):
                                 maxPTA = x_pta 
                 return [articlePicked]
  
+	def decide(self, pool_articles, userID, k = 1):
+		# MEAN
+		art_features = np.empty([len(pool_articles), len(pool_articles[0].contextFeatureVector[:self.dimension])])
+		for i in range(len(pool_articles)):
+			art_features[i, :] = pool_articles[i].contextFeatureVector[:self.dimension]
+		user_features = self.users[userID].UserTheta
+
+		# VARIANCE
+		pta_matrix = np.dot(art_features, user_features) 
+
+
+		pool_positions = np.argsort(pta_matrix)[(k*-1):]
+		articles = []
+		for i in range(k):
+			articles.append(pool_articles[pool_positions[i]])
+		return articles
+
         def updateParameters(self, article_picked, click, userID):
                 self.users[userID].updateParameters(article_picked.contextFeatureVector[:self.dimension], click)
 
         def getTheta(self, userID):
-                return self.users[userID].theta 
+                return self.users[userID].UserTheta 
